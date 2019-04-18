94289fc19b2b1727984bdb2a9f47bad76282fafa
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-18 21:56:25 +1000

    Add async call to sandbox

--- tests/sandbox.ts
+++ tests/sandbox.ts
@@ -1,4 +1,7 @@
 import { shell } from "../src/index"
 
-console.log("shell call: ts-node ./tests/script.ts")
+console.log("sync shell call: ts-node ./tests/script.ts")
 shell("ts-node ./tests/script.ts")
+
+console.log("async shell call: ts-node ./tests/script.ts")
+shell("ts-node ./tests/script.ts", { async: true })
