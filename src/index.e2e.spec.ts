b1f09c977749beff140b2f7c195b3eb27a205e39
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-23 19:18:23 +1000

    Add more e2e tests for async version

--- src/index.e2e.spec.ts
+++ src/index.e2e.spec.ts
@@ -1,4 +1,4 @@
-import { IShellOptions, shell } from "./index"
+import { IShellOptions, prefixTransform, shell } from "./index"
 
 describe("shell()", () => {
   let options: IShellOptions
@@ -20,22 +20,59 @@ describe("shell()", () => {
   })
 
   describe("with async=false option", () => {
-    describe("and rest of options are default", () => {
+    it("returns command output", () => {
+      expect(shell("echo 'command output'", options)).toEqual(
+        "command output\n"
+      )
+    })
+
+    it("writes command output to parent process", () => {
+      shell("echo 'command output'", options)
+      expect(parentProcess.stdout.write).toHaveBeenCalledWith(
+        "command output\n"
+      )
+    })
+
+    it("throws error if shell command fails", () => {
+      expect(() => shell("exit 1", options)).toThrow("Command failed: exit 1")
+    })
+
+    describe("and option silent=true", () => {
+      beforeEach(() => {
+        options.silent = true
+      })
+
+      it("does not write to parent process", () => {
+        shell("echo 'command output'", options)
+        expect(parentProcess.stdout.write).not.toHaveBeenCalled()
+      })
+
       it("returns command output", () => {
         expect(shell("echo 'command output'", options)).toEqual(
           "command output\n"
         )
       })
+    })
 
-      it("writes command output to parent process", () => {
-        shell("echo 'command output'", options)
-        expect(parentProcess.stdout.write).toHaveBeenCalledWith(
-          "command output\n"
-        )
+    describe("and option nopipe=true", () => {
+      beforeEach(() => {
+        options.nopipe = true
       })
 
-      it("throws error if shell command fails", () => {
-        expect(() => shell("exit 1", options)).toThrow("Command failed: exit 1")
+      it("does not return command output", () => {
+        expect(shell("echo 'command output'", options)).toBeNull()
+      })
+    })
+
+    describe("and option transform given", () => {
+      beforeEach(() => {
+        options.transform = prefixTransform("[prefix]")
+      })
+
+      it("transforms command output", () => {
+        expect(shell("echo 'command output'", options)).toEqual(
+          "[prefix] command output\n"
+        )
       })
     })
   })
