e382042373b24bdefa7583563d7eec976aab19d0
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-23 21:16:05 +1000

    Improve sandbox script

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -20,6 +20,18 @@ async function main() {
     async: true,
     transform: prefixTransform(chalk.bgBlueBright("[prefix-async]"))
   })
+
+  console.log("sync nopipe:")
+  shell(scriptPath, { nopipe: true })
+
+  console.log("async nopipe:")
+  await shell(scriptPath, { async: true, nopipe: true })
+
+  console.log("sync nopipe silent:")
+  shell(scriptPath, { nopipe: true, silent: true })
+
+  console.log("async nopipe silent:")
+  await shell(scriptPath, { async: true, nopipe: true, silent: true })
 }
 
 main()
