4f31d8c94560e10fc976caa00091a73704036b8c
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-23 22:06:24 +1000

    Add multilines to sandbox script

--- sandbox/color.ts
+++ sandbox/color.ts
@@ -1,3 +1,4 @@
 import chalk from "chalk"
 
-console.log(chalk.yellow("Text in yellow color"))
+console.log(chalk.yellow("Text in yellow color 1"))
+console.log(chalk.yellow("Text in yellow color 2"))
