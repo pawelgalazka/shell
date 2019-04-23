be658e090b50428dd58d2e7c7efa38aa1438e4ea
Paweł Gałązka <pawel.galazka1986@gmail.com>
2019-04-23 17:42:23 +1000

    Change tests directory to sandbox

--- sandbox/sandbox.ts
+++ sandbox/sandbox.ts
@@ -2,12 +2,12 @@ import chalk from "chalk"
 import { shell } from "../src/index"
 
 console.log("sync shell call: ts-node ./tests/script.ts")
-shell("ts-node ./tests/script.ts", {
+shell("ts-node ./sandbox/script.ts", {
   transform: output => chalk.bgBlueBright("[test-sync]") + " " + output
 })
 
 console.log("async shell call: ts-node ./tests/script.ts")
-shell("ts-node ./tests/script.ts", {
+shell("ts-node ./sandbox/script.ts", {
   async: true,
   transform: output => chalk.bgBlueBright("[test-async]") + " " + output
 })
